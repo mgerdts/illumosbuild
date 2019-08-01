49ba0528dddbac454bad7ec012f1dadca14f6fa0
Mike Gerdts <mike.gerdts@joyent.com>
2019-08-01 12:03:06 -0500

    override compilers from illumos.sh

--- omni.sh
+++ omni.sh
@@ -10,6 +10,10 @@ export TOOLS_PYTHON=$PYTHON3
 export ON_CLOSED_BINS=/opt/onbld/closed
 
 export GNUC_ROOT=/opt/gcc-7
+export PRIMARY_CC=gcc7,$GNUC_ROOT/bin/gcc,gnu
+export PRIMARY_CCC=gcc7,$GNUC_ROOT/bin/g++,gnu
+export SHADOW_CCS=gcc4,/opt/gcc/4.4.4/bin/gcc,gnu
+export SHADOW_CCCS=gcc4,/opt/gcc/4.4.4/bin/g++,gnu
 
 export ONNV_BUILDNUM=`grep '^VERSION=r' /etc/os-release | cut -c10-`
 export PKGVERS_BRANCH=$ONNV_BUILDNUM.0
